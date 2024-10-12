4c1ea7613120205414e58d44d33699dfb42bfef9
Ziare <zuaresuddith@gmail.com>
2024-10-12 11:12:48 -0400

    cloned all vue stuff into project stuff

---
+++ Client/src/pages/Index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'HomeIndex'
+})
+</script>
+
+<template>
+  <main>
+    <h1>Home</h1>
+    <p>Welcome to your Vue.js app!</p>
+  </main>
+</template>
---
+++ Client/src/pages/Jobs.vue
@@ -0,0 +1,12 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="title">The Jobs Page</h1>
+    <h2 class="subtitle">This is an example of a jobs page</h2>
+    <p>Here is a list of jobs</p>
+  </div>
+</template>
+
+<style scoped></style>
