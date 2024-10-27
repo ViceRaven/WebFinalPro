4aea6c9e15931d13369614d00c9be4b69815ae58
Ziare <zuaresuddith@gmail.com>
2024-10-27 19:50:41 -0400

    basic stuff

--- Client/typed-router.d.ts
+++ Client/typed-router.d.ts
@@ -19,11 +19,13 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
-    '/About': RouteRecordInfo<'/About', '/About', Record<never, never>, Record<never, never>>,
     '/Admin': RouteRecordInfo<'/Admin', '/Admin', Record<never, never>, Record<never, never>>,
     '/Chloe': RouteRecordInfo<'/Chloe', '/Chloe', Record<never, never>, Record<never, never>>,
     '/Contact/Index': RouteRecordInfo<'/Contact/Index', '/Contact/Index', Record<never, never>, Record<never, never>>,
     '/ExerciseLog': RouteRecordInfo<'/ExerciseLog', '/ExerciseLog', Record<never, never>, Record<never, never>>,
+    '/ManageProfileChloe': RouteRecordInfo<'/ManageProfileChloe', '/ManageProfileChloe', Record<never, never>, Record<never, never>>,
+    '/ManageProfileMax': RouteRecordInfo<'/ManageProfileMax', '/ManageProfileMax', Record<never, never>, Record<never, never>>,
+    '/ManageProfileRachel': RouteRecordInfo<'/ManageProfileRachel', '/ManageProfileRachel', Record<never, never>, Record<never, never>>,
     '/Max': RouteRecordInfo<'/Max', '/Max', Record<never, never>, Record<never, never>>,
     '/Rachel': RouteRecordInfo<'/Rachel', '/Rachel', Record<never, never>, Record<never, never>>,
     '/Social': RouteRecordInfo<'/Social', '/Social', Record<never, never>, Record<never, never>>,
