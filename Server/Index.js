262349cf955f3c5c8165e92ae1e5729465de402b
ViceRaven <suddithz1@newpaltz.edu>
2024-11-10 11:40:40 -0500

    added stuff from class

---
+++ Server/Index.js
@@ -0,0 +1,27 @@
+/*  B"H
+ */
+const express = require("express");
+const app = express();
+const userController = require("./controllers/users");
+const productController = require("./controllers/products");
+
+const PORT = 3001;
+
+// Middleware
+app.use(express.json());
+app.use(express.static(__dirname + "/dist"));
+
+// Controllers
+app
+  .get("/", (req, res) => {
+    res.send("Hello World");
+  })
+  .get("/about", (req, res) => {
+    res.send("About Us");
+  })
+  .use("/users", userController)
+  .use("/products", productController);
+
+app.listen(PORT, () => {
+  console.log("Server is running at http://localhost:" + PORT);
+});
